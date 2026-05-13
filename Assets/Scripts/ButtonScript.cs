758037d8dc32b14c3392120370b801a234efcae1
Hexagrim <hexagrim3456@gmail.com>
2026-05-13 20:53:06 +0545

    Hour 6, made more fixes, finished buttons, fixed plyer animations, fixed ragdoll being too sloppy and made better feel

--- Assets/Scripts/ButtonScript.cs
+++ Assets/Scripts/ButtonScript.cs
@@ -2,15 +2,42 @@ using UnityEngine;
 
 public class ButtonScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject ConnectedBehaviour;
+
+
+    bool pressed;
+    private Animator Anim;
+
+    int pressingObjs = 0;
     void Start()
     {
-        
+        Anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        pressed = pressingObjs != 0;
+
+        if(pressingObjs < 0)
+        {
+            pressingObjs = 0;
+        }
+        Anim.SetBool("pressing", pressed);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player") || collision.CompareTag("Corpse"))
+        {
+            pressingObjs++;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") || collision.CompareTag("Corpse"))
+        {
+            pressingObjs--;
+        }
     }
 }
